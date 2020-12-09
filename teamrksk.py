0bd398e766ec0d6225fce511eed813952e355c07
TeamRKSKgang <75716691+TeamRKSKgang@users.noreply.github.com>
2020-12-09 15:19:36 +0600

    Update and rename zeroboy.py to teamrksk.py

--- teamrksk.py
+++ teamrksk.py
@@ -87,8 +87,8 @@ print  """\033[1;91m=======================================
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "team"
+CorrectPassword = "RKSK"
 
 loop = 'true'
 while (loop == 'true'):
